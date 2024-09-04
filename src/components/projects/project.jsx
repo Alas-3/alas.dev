bed3a38362d281a7cdf0ef042463541e4f3a5bca
Alas-3 <ac3labador@gmail.com>
2024-09-04 04:06:58 +0000

    adjusted projects page & optimized about page logo

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -6,32 +6,32 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 import "./styles/project.css";
 
 const Project = (props) => {
-	const { logo, logo2, logo3, logo4, title, description, linkText, link } = props;
+    const { logo, logo2, logo3, logo4, title, description, linkText, link } = props;
 
-	return (
-		<React.Fragment>
-			<div className="project">
-				<Link to={link} target="_blank" rel="noopener noreferrer">
-					<div className="project-container">
-						<div className="project-logo">
-							{logo && <img src={logo} alt="logo" />}
-							{logo2 && <img src={logo2} alt="logo2" />}
-							{logo3 && <img src={logo3} alt="logo3" />}
-							{logo4 && <img src={logo4} alt="logo4" />}
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-							<div className="project-link-icon">
-								<FontAwesomeIcon icon={faLink} />
-							</div>
-							<div className="project-link-text">{linkText}</div>
-						</div>
-					</div>
-				</Link>
-			</div>
-		</React.Fragment>
-	);
+    return (
+        <React.Fragment>
+            <div className="project">
+                <Link to={link} target="_blank" rel="noopener noreferrer">
+                    <div className="project-container">
+                        <div className="project-title">{title}</div>
+                        <div className="project-description">{description}</div>
+                        <div className="project-logo">
+                            {logo && <img src={logo} alt="logo" />}
+                            {logo2 && <img src={logo2} alt="logo2" />}
+                            {logo3 && <img src={logo3} alt="logo3" />}
+                            {logo4 && <img src={logo4} alt="logo4" />}
+                        </div>
+                        <div className="project-link">
+                            <div className="project-link-icon">
+                                <FontAwesomeIcon icon={faLink} />
+                            </div>
+                            <div className="project-link-text">{linkText}</div>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default Project;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,116 +9,137 @@ import Socials from "../components/about/socials";
 import INFO from "../data/user";
 import SEO from "../data/seo";
 
-import CERTIFICATES from "../data/certificates"; 
+import CERTIFICATES from "../data/certificates";
 
 import "./styles/about.css";
 
 const About = () => {
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-	const currentSEO = SEO.find((item) => item.page === "about");
+  const currentSEO = SEO.find((item) => item.page === "about");
 
-	return (
-		<React.Fragment>
-			<Helmet>
-				<title>{`About | ${INFO.main.title}`}</title>
-				<meta name="description" content={currentSEO.description} />
-				<meta
-					name="keywords"
-					content={currentSEO.keywords.join(", ")}
-				/>
-			</Helmet>
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>{`About | ${INFO.main.title}`}</title>
+        <meta name="description" content={currentSEO.description} />
+        <meta
+          name="keywords"
+          content={currentSEO.keywords.join(", ")}
+        />
+        <link
+          rel="preload"
+          href="https://github.com/Alas-3/Alas-3/blob/main/personal_logo_withoutBG.png?raw=true"
+          as="image"
+        />
+      </Helmet>
 
-			<div className="page-content">
-				<NavBar active="about" />
-				<div className="content-wrapper">
-					<div className="about-logo-container">
-						<div className="about-logo">
-							<Logo width={46} />
-						</div>
-					</div>
+      <div className="page-content">
+        <NavBar active="about" />
+        <div className="content-wrapper">
+          <div className="about-logo-container">
+            <div className="about-logo">
+              <Logo width={46} />
+            </div>
+          </div>
 
-					<div className="about-container">
-						<div className="about-main">
-							<div className="about-right-side">
-								<div className="title about-title">
-									{INFO.about.title}
-								</div>
+          <div className="about-container">
+            <div className="about-main">
+              <div className="about-right-side">
+                <div className="title about-title">
+                  {INFO.about.title}
+                </div>
 
-								<div className="subtitle about-subtitle">
-									{INFO.about.description}
-								</div>
-							</div>
+                <div className="subtitle about-subtitle">
+                  {INFO.about.description}
+                </div>
+              </div>
 
-							<div className="about-left-side">
-								<div className="about-image-container">
-									<div className="about-image-wrapper">
-										<img
-											src="https://github.com/Alas-3/Alas-3/blob/main/personal_logo_withoutBG.png?raw=true"
-											alt="about"
-											className="about-image"
-										/>
-									</div>
-								</div>
+              <div className="about-left-side">
+                <div className="about-image-container">
+                  <div className="about-image-wrapper">
+                    <img
+                      src="https://github.com/Alas-3/Alas-3/blob/main/personal_logo_withoutBG.png?raw=true"
+                      alt="about"
+                      className="about-image"
+                      loading="lazy"
+                    />
+                  </div>
+                </div>
 
-								<div className="about-socials">
-									<Socials />
-								</div>
-							</div>
-						</div>
-						<div className="about-socials-mobile">
-							<Socials />
-						</div>
-						<div className="certificates-section">
-							<h2>My Certifications</h2>
-							<div className="certificates-grid">
-								{CERTIFICATES.map((cert, index) => {
-									const CardContent = (
-										<div className="card-container">
-											<div className="certificate-info">
-												<div className="certificate-header">
-													{cert.logo && (
-														<img src={cert.logo} alt={`${cert.issuer} logo`} className="certificate-logo" />
-													)}
-													<div>
-														<div className="certificate-title">{cert.title}</div>
-														<div className="certificate-issuer">
-															Issued by: <span className="bold-text">{cert.issuer}</span>  |  
-															Issued: <span className="bold-text">{cert.date}</span>
-														</div>
-													</div>
-												</div>
-												
-												{cert.description && (
-													<div className="certificate-description">{cert.description}</div>
-												)}
-											</div>
-										</div>
-									);
+                <div className="about-socials">
+                  <Socials />
+                </div>
+              </div>
+            </div>
+            <div className="about-socials-mobile">
+              <Socials />
+            </div>
+            <div className="certificates-section">
+              <h2>My Certificates</h2>
+              <div className="certificates-grid">
+                {CERTIFICATES.map((cert, index) => {
+                  const CardContent = (
+                    <div className="card-container">
+                      <div className="certificate-info">
+                        <div className="certificate-header">
+                          {cert.logo && (
+                            <img
+                              src={cert.logo}
+                              alt={`${cert.issuer} logo`}
+                              className="certificate-logo"
+                            />
+                          )}
+                          <div>
+                            <div className="certificate-title">
+                              {cert.title}
+                            </div>
+                            <div className="certificate-issuer">
+                              Issued by:{" "}
+                              <span className="bold-text">{cert.issuer}</span>{" "}
+                              | Issued:{" "}
+                              <span className="bold-text">{cert.date}</span>
+                            </div>
+                          </div>
+                        </div>
 
-									return cert.link ? (
-										<a key={index} href={cert.link} target="_blank" rel="noopener noreferrer" className="card"> 
-											{CardContent}
-										</a>
-									) : (
-										<div key={index} className="card">
-											{CardContent}
-										</div>
-									);
-								})}
-							</div>
-						</div>
+                        {cert.description && (
+                          <div className="certificate-description">
+                            {cert.description}
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  );
 
-					</div>
-					<div className="page-footer">
-						<Footer />
-					</div>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+                  return cert.link ? (
+                    <a
+                      key={index}
+                      href={cert.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="card"
+                    >
+                      {CardContent}
+                    </a>
+                  ) : (
+                    <div key={index} className="card">
+                      {CardContent}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          <div className="page-footer">
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default About;
