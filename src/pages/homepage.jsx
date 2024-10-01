2b6a83fd9a8ac38fb2b030ae46fff0d075ed11de
Alas-3 <ac3labador@gmail.com>
2024-10-01 10:14:13 +0000

    replaced old resume & re-formatted code for readability in homepage file

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -6,12 +6,12 @@ import { Helmet } from "react-helmet";
 import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faFacebook,
-  faLinkedin,
-  faGithub,
-  //faStackOverflow,
-  //faInstagram,
-  //faTwitter,
+	faFacebook,
+	faLinkedin,
+	faGithub,
+	//faStackOverflow,
+	//faInstagram,
+	//faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 
 import { faCertificate } from "@fortawesome/free-solid-svg-icons";
@@ -29,154 +29,180 @@ import SEO from "../data/seo";
 import "./styles/homepage.css";
 
 const Homepage = () => {
-  const [stayLogo, setStayLogo] = useState(false);
-  const [logoSize, setLogoSize] = useState(80);
-  const [oldLogoSize, setOldLogoSize] = useState(80);
+	const [stayLogo, setStayLogo] = useState(false);
+	const [logoSize, setLogoSize] = useState(80);
+	const [oldLogoSize, setOldLogoSize] = useState(80);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      let scroll = Math.round(window.pageYOffset, 2);
-      let newLogoSize = 80 - (scroll * 4) / 10;
+	useEffect(() => {
+		const handleScroll = () => {
+			let scroll = Math.round(window.pageYOffset, 2);
+			let newLogoSize = 80 - (scroll * 4) / 10;
 
-      if (newLogoSize < oldLogoSize) {
-        if (newLogoSize > 40) {
-          setLogoSize(newLogoSize);
-          setOldLogoSize(newLogoSize);
-          setStayLogo(false);
-        } else {
-          setStayLogo(true);
-        }
-      } else {
-        setLogoSize(newLogoSize);
-        setStayLogo(false);
-      }
-    };
+			if (newLogoSize < oldLogoSize) {
+				if (newLogoSize > 40) {
+					setLogoSize(newLogoSize);
+					setOldLogoSize(newLogoSize);
+					setStayLogo(false);
+				} else {
+					setStayLogo(true);
+				}
+			} else {
+				setLogoSize(newLogoSize);
+				setStayLogo(false);
+			}
+		};
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [logoSize, oldLogoSize]);
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, [logoSize, oldLogoSize]);
 
-  const currentSEO = SEO.find((item) => item.page === "home");
+	const currentSEO = SEO.find((item) => item.page === "home");
 
-  const logoStyle = {
-    display: "flex",
-    position: stayLogo ? "fixed" : "relative",
-    top: stayLogo ? "3vh" : "auto",
-    zIndex: 999,
-    border: stayLogo ? "1px solid white" : "none",
-    borderRadius: stayLogo ? "50%" : "none",
-    boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
-  };
-  
-  // Keywords to highlight
-  const keywords = ["HTML", "CSS", "JavaScript", "React", "Next.js", "Tailwind", "Firebase", "Supabase", "Web", "Developer"];
-  const highlightColor = "#14b8a6"; // Define the color for highlighted text
+	const logoStyle = {
+		display: "flex",
+		position: stayLogo ? "fixed" : "relative",
+		top: stayLogo ? "3vh" : "auto",
+		zIndex: 999,
+		border: stayLogo ? "1px solid white" : "none",
+		borderRadius: stayLogo ? "50%" : "none",
+		boxShadow: stayLogo ? "0px 4px 10px rgba(0, 0, 0, 0.25)" : "none",
+	};
 
-  return (
-    <React.Fragment>
-      <Helmet>
-        <title>{INFO.main.title}</title>
-        <meta name="description" content={currentSEO.description} />
-        <meta name="keywords" content={currentSEO.keywords.join(", ")} />
-      </Helmet>
+	// Keywords to highlight
+	const keywords = [
+		"HTML",
+		"CSS",
+		"JavaScript",
+		"React",
+		"Next.js",
+		"Tailwind",
+		"Firebase",
+		"Supabase",
+		"Web",
+		"Developer",
+	];
+	const highlightColor = "#14b8a6"; // Define the color for highlighted text
 
-      <div className="page-content">
-        <NavBar active="home" />
-        <div className="content-wrapper">
-          <div className="homepage-logo-container">
-            <div style={logoStyle}>
-              <Logo width={logoSize} link={false} />
-            </div>
-          </div>
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{INFO.main.title}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
 
-          <div className="homepage-container">
-            <div className="homepage-first-area">
-              <div className="homepage-first-area-left-side">
-              <div className="title homepage-title">
-                  <HighlightedText
-                    text={INFO.homepage.title}
-                    keywords={keywords}
-                    highlightColor={highlightColor}
-                  />
-                </div>
+			<div className="page-content">
+				<NavBar active="home" />
+				<div className="content-wrapper">
+					<div className="homepage-logo-container">
+						<div style={logoStyle}>
+							<Logo width={logoSize} link={false} />
+						</div>
+					</div>
 
-                <div className="subtitle homepage-subtitle">
-                  <HighlightedText
-                    text={INFO.homepage.description}
-                    keywords={keywords}
-                    highlightColor={highlightColor}
-                  />
-                </div>
-              </div>
-            </div>
+					<div className="homepage-container">
+						<div className="homepage-first-area">
+							<div className="homepage-first-area-left-side">
+								<div className="title homepage-title">
+									<HighlightedText
+										text={INFO.homepage.title}
+										keywords={keywords}
+										highlightColor={highlightColor}
+									/>
+								</div>
 
-            <div className="homepage-socials">
-              <a
-                href="assets/Resume.pdf"
-                target="_blank"
-                rel="noreferrer"
-                className="resume-button"
-              >
-                Resume
-              </a>
+								<div className="subtitle homepage-subtitle">
+									<HighlightedText
+										text={INFO.homepage.description}
+										keywords={keywords}
+										highlightColor={highlightColor}
+									/>
+								</div>
+							</div>
+						</div>
 
-              <span className="social-separator">|</span>
+						<div className="homepage-socials">
+							<a
+								href="assets/ResumeV2.pdf"
+								target="_blank"
+								rel="noreferrer"
+								className="resume-button"
+							>
+								Resume
+							</a>
 
-              <a href={INFO.socials.credly} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon
-                  icon={faCertificate}
-                  className="homepage-social-icon"
-                />
-              </a>
+							<span className="social-separator">|</span>
 
-              <a href={INFO.socials.linkedin} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon
-                  icon={faLinkedin}
-                  className="homepage-social-icon"
-                />
-              </a>
+							<a
+								href={INFO.socials.credly}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faCertificate}
+									className="homepage-social-icon"
+								/>
+							</a>
 
-              <a href={INFO.socials.github} target="_blank" rel="noreferrer">
-                <FontAwesomeIcon
-                  icon={faGithub}
-                  className="homepage-social-icon"
-                />
-              </a>
+							<a
+								href={INFO.socials.linkedin}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faLinkedin}
+									className="homepage-social-icon"
+								/>
+							</a>
 
-              <a
-                href={`mailto:${INFO.main.email}`}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FontAwesomeIcon
-                  icon={faMailBulk}
-                  className="homepage-social-icon"
-                />
-              </a>
-            </div>
+							<a
+								href={INFO.socials.github}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faGithub}
+									className="homepage-social-icon"
+								/>
+							</a>
 
-            <div className="homepage-techstack">
-              <TechStack />
-            </div>
+							<a
+								href={`mailto:${INFO.main.email}`}
+								target="_blank"
+								rel="noreferrer"
+							>
+								<FontAwesomeIcon
+									icon={faMailBulk}
+									className="homepage-social-icon"
+								/>
+							</a>
+						</div>
 
-            <div className="homepage-after-title">
-              <div className="homepage-works">
-                <Works />
-              </div>
-            </div>
+						<div className="homepage-techstack">
+							<TechStack />
+						</div>
 
-            <div className="page-footer">
-              <Footer />
-            </div>
-          </div>
-        </div>
-      </div>
-    </React.Fragment>
-  );
+						<div className="homepage-after-title">
+							<div className="homepage-works">
+								<Works />
+							</div>
+						</div>
+
+						<div className="page-footer">
+							<Footer />
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Homepage;
