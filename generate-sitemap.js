bbb21388729435c86ba1631417acc4bf58b5b3b3
ac3labador <ac3labador@gmail.com>
2024-10-10 20:58:28 +0000

    changed sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const sitemapStream = new SitemapStream({ hostname: 'https://alas-dev.vercel.app' });
 
 // Add URLs to the sitemap
-sitemapStream.write({ url: '/', changefreq: 'monthly', priority: 1.0, lastmod: new Date().toISOString() });
-sitemapStream.write({ url: '/about', changefreq: 'monthly', priority: 0.8, lastmod: new Date().toISOString() });
-sitemapStream.write({ url: '/projects', changefreq: 'monthly', priority: 0.8, lastmod: new Date().toISOString() });
+sitemapStream.write({ url: '/', changefreq: 'hourly', priority: 1.0, lastmod: new Date().toISOString() });
+sitemapStream.write({ url: '/about', changefreq: 'hourly', priority: 0.8, lastmod: new Date().toISOString() });
+sitemapStream.write({ url: '/projects', changefreq: 'hourly', priority: 0.8, lastmod: new Date().toISOString() });
 sitemapStream.end();
 
 // Write to sitemap.xml file
